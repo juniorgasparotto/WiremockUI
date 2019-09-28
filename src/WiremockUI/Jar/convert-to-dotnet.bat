2560da93017b37ead066043784d7fd037e0b2e0b
Glauber Donizeti Gasparotto Junior <juniorgasparotto@msn.com>
2019-09-28 00:05:37 -0300

    new version start - update ikvm,wiremock,.netframework - resolve same issues

--- src/WiremockUI/Jar/convert-to-dotnet.bat
+++ src/WiremockUI/Jar/convert-to-dotnet.bat
@@ -1,1 +1,2 @@
-"D:\ikvm-7.2.4630.5\bin\ikvmc" -target:library "wiremock-standalone-2.8.0.jar"
+unzip "ikvmbin-8.1.5717.0.zip" -d "C:/"
+"C:\ikvm-8.1.5717.0\bin\ikvmc" -target:library "wiremock-standalone-2.8.0.jar"
